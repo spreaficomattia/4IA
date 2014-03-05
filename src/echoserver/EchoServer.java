403018f8e072e9093c753a3cf1b2eabdd234d2a9
spreafico.mattia <spreafico.mattia@tave15.OSDBSESTO.local>
2014-03-05 11:31:29 +0100

    Prova di commit

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,9 +7,10 @@ public class EchoServer {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket ss = new ServerSocket(6666);
+        ServerSocket ss = new ServerSocket(2222);
         while (true) {
             new EchoThread(ss.accept()).start();
+            //COMMENTO
         }
     }
 }
